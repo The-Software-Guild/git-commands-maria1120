77979bb2d28eb7edbd55ee1a6af1192d05b5933c
Maria Miteva <mpmiteva@yahoo.com>
2022-10-12 14:06:11 +0100

    Practicing programming

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/ProjectGutenburg.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.basics;
+
+/**
+ *
+ * @author mpmit
+ */
+public class ProjectGutenburg {
+    
+       public static void projectGutenburg(){
+       
+       System.out.print("Did you know that in 1440 (or thereabouts),");
+        System.out.print("Johannes Gutenberg invented the printing press?");
+        System.out.print("He started out as a goldsmith!");
+        System.out.print("His invention made it easy to print and");
+        System.out.print("distribute books to anyone who wanted one.");
+        System.out.print("We are like a modern Gutenberg,");
+        System.out.print("printing vast amounts to the waiting console with ease.");
+       
+       }
+    
+    
+    
+    
+    
+    
+}
---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/InABucket.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author mpmit
+ */
+public class InABucket {
+    
+    public static void inABucket(){
+    // You can declare all KINDS of variables.
+        String walrus;
+        double piesEaten;
+        float weightOfTeacupPig;
+        int grainsOfSand;
+
+        // But declaring them just sets up the space for data
+        // to use the variable, you have to put data IN it first!
+        walrus = "Sir Leroy Jenkins III";
+        piesEaten = 42.1;
+        weightOfTeacupPig = 1.23f;
+        grainsOfSand = 2;
+
+        System.out.println("Meet my pet walrus, " + walrus);
+        System.out.print("He was a bit hungry today, and ate this many pies : ");
+        System.out.println(piesEaten);
+        System.out.println("The weight of Tea cup pig is: " + weightOfTeacupPig);
+        System.out.println("Grains of sands: " + grainsOfSand);
+    }
+    
+}
